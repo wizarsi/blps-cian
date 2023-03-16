b3ac7202811ce752d957b8441d1c2af98c472a09
wizarsi <>
2023-03-17 00:23:23 +0300

    Initial commit

---
+++ src/main/java/com/example/blpscian/BlpsCianApplication.java
@@ -0,0 +1,13 @@
+package com.example.blpscian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlpsCianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlpsCianApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/blpscian/BlpsCianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blpscian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlpsCianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
