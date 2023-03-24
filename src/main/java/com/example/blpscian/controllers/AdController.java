4d4aa626e0dfb368513443d5715d65f9291684f1
wizarsi <>
2023-03-24 22:11:49 +0300

    Все эндпоинты для бизнес процесса "Найти объявления"

--- src/main/java/com/example/blpscian/controllers/AdController.java
+++ src/main/java/com/example/blpscian/controllers/AdController.java
@@ -1,32 +1,27 @@
 package com.example.blpscian.controllers;
 
-import com.example.blpscian.model.AdCommercial;
-import com.example.blpscian.model.AdResidential;
 import com.example.blpscian.model.dto.AdCommercialDto;
 import com.example.blpscian.model.dto.AdResidentialDto;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @Controller
 @RequestMapping("ad")
 public class AdController {
     @PostMapping(value = "search-commercial")
-    public ResponseEntity<?> searchCommercial(@RequestBody AdCommercialDto) {
-
+    public ResponseEntity<?> searchCommercial(@RequestBody AdCommercialDto adCommercialDto) {
+        return (ResponseEntity<?>) ResponseEntity.ok();
     }
 
     @PostMapping(value = "search-residential")
-    public ResponseEntity<?> searchResidential(@RequestBody AdResidentialDto) {
-
+    public ResponseEntity<?> searchResidential(@RequestBody AdResidentialDto adResidentialDto) {
+        return (ResponseEntity<?>) ResponseEntity.ok();
     }
 
     @GetMapping(value = "get-locations")
     public ResponseEntity<?> getLocations() {
-
+        return (ResponseEntity<?>) ResponseEntity.ok();
     }
 }
